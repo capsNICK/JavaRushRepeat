81004ab154b062edb07522feeed89d35cc0d315f
capsNICK <alexandervmalysh@gmail.com>
2023-02-06 23:03:39 +0500

    Small edits in syntax 0105 and 0201

--- src/tasks01/solution0105.java
+++ src/tasks01/solution0105.java
@@ -3,6 +3,6 @@ package tasks01;
 public class solution0105 {
     public static void main(String[] args) {
         System.out.println("Сбрасываем Диего");
-        System.out.printf("Сбрасываем сверхсекретный груз");
+        System.out.print("Сбрасываем сверхсекретный груз");
     }
 }
--- src/tasks02/solution0201.java
+++ src/tasks02/solution0201.java
@@ -2,7 +2,7 @@ package tasks02;
 
 public class solution0201 {
     public static void main(String[] args) {
-        int id = 001, age = 20;
+        int id = 1, age = 20;
         String documents = "passport",behavior = "criminal";
     }
 }
