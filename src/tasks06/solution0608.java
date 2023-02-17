341c7de94f6b533f3734856309fc2962ccce281c
capsNICK <alexandervmalysh@gmail.com>
2023-02-17 19:47:59 +0500

    Solutions 0608 - to do.

---
+++ src/tasks06/solution0608.java
@@ -0,0 +1,20 @@
+package tasks06;
+
+import java.util.Scanner;
+
+//reverse
+
+public class solution0608 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = console.nextInt();
+        } if (n % 2 == 0) {
+            for (int i = n - 1; i >) //todo
+        }
+
+    }
+}
